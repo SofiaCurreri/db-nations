2bd30bb1733ec3e131e7830bb389dd90835628cf
SofiaCurreri <sofiacurreri@gmail.com>
2023-06-19 09:19:42 +0200

    milestone 3 completed

--- src/org/lessons/java/database/Main.java
+++ src/org/lessons/java/database/Main.java
@@ -28,7 +28,7 @@ public class Main {
                     FROM `countries`
                     JOIN regions ON countries.region_id = regions.region_id
                     JOIN continents ON regions.continent_id = continents.continent_id
-                    WHERE countries.name LIKE "%?%" OR regions.name LIKE "%?%" OR continents.name LIKE "%?%"
+                    WHERE countries.name LIKE ? OR regions.name LIKE ? OR continents.name LIKE ?
                     ORDER BY countries.name ASC;
                     """;
 
@@ -51,6 +51,7 @@ public class Main {
                 Scanner utente = new Scanner(System.in);
                 System.out.println("Inserisci stringa da ricercare: ");
                 String ricerca = utente.nextLine();
+                ricerca = "%" + ricerca + "%";
 
                 ps.setString(1,ricerca);
                 ps.setString(2,ricerca);
@@ -68,9 +69,7 @@ public class Main {
                     }
                 }
             }
-            Scanner utente = new Scanner(System.in);
-            System.out.println("Inserisci stringa da ricercare: ");
-            String ricerca = utente.nextLine();
+
         }catch (SQLException e){
             System.out.println("Unable to connect to database");
             e.printStackTrace();
